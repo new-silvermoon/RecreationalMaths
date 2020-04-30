e7ff5861af1a2f70ea7ab2d1f12e4a2ce7ae6000
Sagar Das <sagardas@commercecx.com>
2020-04-29 23:20:26 -0400

    Permutation Generator
    Added code for Next lexicographical permuation algorithm

---
+++ PermutationsGenerator.py
@@ -0,0 +1,87 @@
+import copy
+
+
+class PermutationGenerator:
+    list_of_elements = []
+    permutation_indices = None
+    tot_num_permutations = 0
+    remaining_permutations = 0
+
+    def __init__(self, list_of_elements):
+        self.list_of_elements = copy.deepcopy(list_of_elements)
+        self.permutation_indices = [None] * len(self.list_of_elements)
+        self.tot_num_permutations = self.calculate_factorial(len(self.list_of_elements))
+        self.reset_permutation_generator()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.trigger_permutation()
+
+    def calculate_factorial(self, limit):
+        if limit == 0:
+            return 1
+        return limit * self.calculate_factorial(limit - 1)
+
+    def reset_permutation_generator(self):
+        for i in range(len(self.permutation_indices)):
+            self.permutation_indices[i] = i
+        self.remaining_permutations = self.tot_num_permutations
+
+    def trigger_permutation(self):
+        permutation = [None] * len(self.permutation_indices)
+        return self.generate_permutation(permutation)
+
+    def generate_permutation(self, permutation):
+        if len(permutation) != len(self.list_of_elements):
+            raise ValueError("Size of current permutation array must be equal to the size of list of elements ")
+
+        self.generate_next_permuation_indices_lexicographic()
+
+        for i in range(len(self.permutation_indices)):
+            permutation[i] = self.list_of_elements[self.permutation_indices[i]]
+
+        return permutation
+
+
+    """ https://www.nayuki.io/page/next-lexicographical-permutation-algorithm """
+    def generate_next_permuation_indices_lexicographic(self):
+
+        if self.remaining_permutations == 0:
+            #raise ValueError('No permutations left')
+            raise StopIteration
+        elif self.remaining_permutations < self.tot_num_permutations:
+
+            j = len(self.permutation_indices) - 2
+
+            while self.permutation_indices[j] > self.permutation_indices[j + 1]:
+                j -= 1
+
+            k = len(self.permutation_indices) - 1
+            while (self.permutation_indices[j] > self.permutation_indices[k]):
+                k -= 1
+
+            self.permutation_indices[j], self.permutation_indices[k] = self.permutation_indices[k], \
+                                                                       self.permutation_indices[j]
+
+            r = len(self.permutation_indices) - 1
+            s = j + 1
+
+            while r > s:
+                self.permutation_indices[r], self.permutation_indices[s] = self.permutation_indices[s], \
+                                                                           self.permutation_indices[r]
+                r -= 1
+                s += 1
+
+
+        self.remaining_permutations -= 1
+
+
+
+
+
+permutations = iter(PermutationGenerator(["1", "2", "3"]))
+
+for permutation in iter(PermutationGenerator(["1", "2", "3"])):
+    print(permutation)
--- README.md
+++ README.md
@@ -7,6 +7,9 @@ This repo contains code representing various Maths formulae, puzzles, sequences,
 3. Sum of cubes - There is a popular conjecture in Maths which states that numbers that can be expressed as a sum of three cubes of integers,
  allowing both positive and negative cubes in the sum. Refer https://en.wikipedia.org/wiki/Sums_of_three_cubes
 
- Andrew Booker wrote an algorithm in March 2019 in order to find the result for number 33. Refer https://www.youtube.com/watch?v=ASoz_NuIvP0
+    Andrew Booker wrote an algorithm in March 2019 in order to find the result for number 33. Refer https://www.youtube.com/watch?v=ASoz_NuIvP0
 
- This code takes stab at Andrew's original algorithm in a Pythonic way.
+    This code takes stab at Andrew's original algorithm in a Pythonic way.
+
+4. Permutations generator - 
+    Next Lexicographic permutation algorithm - https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
